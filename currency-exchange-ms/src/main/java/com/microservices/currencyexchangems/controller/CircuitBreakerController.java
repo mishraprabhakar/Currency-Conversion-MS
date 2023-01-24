f7a4aeb316b77429c1694b39dc84b711e2136725
mishraprabhakar <eparis@atlassian.com>
2023-01-24 19:59:07 +0530

    resilience4j introduced to handle Circuit Breaker

---
+++ currency-exchange-ms/src/main/java/com/microservices/currencyexchangems/controller/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package com.microservices.currencyexchangems.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "printHelloWorld")
+    public String sampleApi(){
+        logger.info("sample API is called");
+
+        ResponseEntity<String> forEntity = new RestTemplate()
+                .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+        return forEntity.getBody();
+    }
+
+    public String printHelloWorld(Exception exception){
+        return "Hello World fallback page.";
+    }
+}
